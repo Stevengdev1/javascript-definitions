b670f2c55796e4204b609f08e92e1925069f52d7
Steven <stevendg922@gmail.com>
2023-02-11 18:08:37 -0500

    Added 'Objects, Privacy

--- index.js
+++ index.js
@@ -406,7 +406,23 @@ const robot = {
 
 console.log(robot.provideInfo());
 --------------------------------------------------------------------------------------------------------------
+//PRIVACY
 
+const robot = {
+  _energyLevel: 100,
+  recharge(){
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+  }
+};
+
+robot._energyLevel = 'high';
+
+robot.recharge()
+--------------------------------------------------------------------------------------------------------------
+
+
+ 
 
 
 
