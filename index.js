bfe7a5e57262c562c12cc5927094973dde2ea241
Steven <stevendg922@gmail.com>
2023-02-10 21:39:48 -0500

    Added 'Objects, Property Assignment

--- index.js
+++ index.js
@@ -266,6 +266,24 @@ const checkConsistentOutput = (func, val) => {
 
 console.log(checkConsistentOutput(addTwo, 10));
 --------------------------------------------------------------------------------------------------------------
+//OBJECTS 
+//PROPERTY ASSIGNMENT
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+// Write your code below
+
+spaceship.color = 'glorious gold';
+
+spaceship.numEngines = 9;
+
+delete spaceship['Secret Mission'];
+--------------------------------------------------------------------------------------------------------------
 
 
 
