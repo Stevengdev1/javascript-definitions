981b870f21a83391093ad1573508800d4fb79623
Steven <stevendg922@gmail.com>
2023-02-10 21:44:15 -0500

    Added 'Objects, Pass by Reference

--- index.js
+++ index.js
@@ -334,6 +334,29 @@ let capFave = spaceship.crew.captain['favorite foods'][0];
 
 let firstPassenger = spaceship.passengers[0];
 --------------------------------------------------------------------------------------------------------------
+//PASS BY REFERENCE
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+// Write your code below
+
+let greenEnergy = obj => {
+  obj['Fuel Type'] = 'avocado oil';
+}
+
+let remotelyDisable = obj => {
+  obj.disabled = true;
+}
+
+greenEnergy(spaceship);
+
+remotelyDisable(spaceship);
+
+console.log(spaceship)
+--------------------------------------------------------------------------------------------------------------
 
 
 
