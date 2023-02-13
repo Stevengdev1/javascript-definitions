c6a4f8bec1cc5f38294e83188d33223d35006783
Steven <stevendg922@gmail.com>
2023-02-12 21:18:30 -0500

    Added 'Objects, Setters

--- index.js
+++ index.js
@@ -420,7 +420,7 @@ robot._energyLevel = 'high';
 
 robot.recharge()
 ---------------------------------------------------------------------------------------------------------------
-//GETTERS
+//GETTERS METHODS
 
 const robot = {
   _model: '1E78V2',
@@ -435,7 +435,32 @@ const robot = {
 };
 
 console.log(robot.energyLevel);
+--------------------------------------------------------------------------------------------------------------
+//SETTER METHODS
 
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num) {
+    if (typeof num === 'number' && num >= 0){
+      this._numOfSensors = num;
+    } else {
+      console.log('Pass in a number that is greater than or equal to 0')
+    }   
+  } 
+};
+
+robot.numOfSensors = 100;
+console.log(robot.numOfSensors);
+--------------------------------------------------------------------------------------------------------------
 
 
 
