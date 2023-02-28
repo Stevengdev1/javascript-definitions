13b4c22c0843181acdc456782ae4cbc040879edd
Steven <stevendg922@gmail.com>
2023-02-27 21:28:05 -0500

    Added Class Instances

--- index.js
+++ index.js
@@ -562,6 +562,19 @@ class Surgeon {
     this.department = department;
   }
 }
+--------------------------------------------------------------------------------------------------------------
+CLASS INSTANCES
+
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
+const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
+const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
+
 
 
 
